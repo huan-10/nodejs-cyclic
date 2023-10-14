727bf4b41aa32ae338a63eb64e46d208c50fb431
Bỏ Cái Tay Ra Bạn Ê <hoanvanhuan2k1@gmail.com>
2023-10-14 15:08:26 +0700

    update

--- config/db/index.js
+++ config/db/index.js
@@ -8,7 +8,9 @@ require("dotenv").config();
 // await mongoose.connect("mongodb://127.0.0.1/H8_education_dev");
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_CONNECT_URI);
+    await mongoose.connect(
+      "mongodb+srv://hoangvanhuan2k1:QDrmnza2xIm9upcX@cluster0.usc9ot5.mongodb.net/?retryWrites=true&w=majority"
+    );
 
     // await mongoose.connect("mongodb://127.0.0.1/H8_education_dev");
 
